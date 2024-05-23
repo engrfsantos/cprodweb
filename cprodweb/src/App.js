c805a10ea7ad72c20a0df3fbfb5396913c0c9b20
engrfsantos <eng.rfsantos@gmail.com>
2024-05-23 07:16:53 -0300

    Produto CPRODWEB HTML CSS 1

--- cprodweb/src/App.js
+++ cprodweb/src/App.js
@@ -4,25 +4,12 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">       
-        <img src={logo} className="App-logo" alt="logo" />
-       <h1>Hello World!</h1>
-      </header> 
-      
-      <body>
-         <div>
-            <h1>Controle de Produção</h1>
-            <h2>Menu Principal</h2>
-             <a href="./lancamento.html">Lançamento</a><br></br>
-             <a href="./producoes.html">Producoes</a><br></br>
-             <a href="./login.html">Login </a><br></br>
-             <a href="./altsenha.html">Alterar Senha </a><br></br>
-             <a href="./usuarios.html">Gerenciar Usuários </a><br></br>
-             <a href="./cadprodutos.html">Cad. Produtos </a><br></br>
-        </div>
-      </body>
-      
-    </div> 
+    <header className="App-header">       
+      <img src={logo} className="App-logo" alt="logo" />
+    </header>      
+    <body>
+    </body>    
+  </div> 
      
       
   );
